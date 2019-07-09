494be9de7807a64a13620ac88c3f0d6d67f9b445
Steven Murawski <steven.murawski@gmail.com>
2019-07-09 10:50:39 -0500

    try moving sudo as rvmsudo is weird

--- files/default/scripts/proxies/single/start.sh
+++ files/default/scripts/proxies/single/start.sh
@@ -9,6 +9,9 @@ run_cmd cp $PROXY_TESTS_DIR/proxies/single/squid.conf /etc/squid/squid.conf
 # Deny all traffic except squid
 echo "Setting routes to deny all traffic except ssh and squid ..."
 run_cmd iptables -F OUTPUT
+run_cmd iptables -A OUTPUT -j ACCEPT -p tcp -dport 443 --dst-range 13.107.6.0-13.107.6.254
+run_cmd iptables -A OUTPUT -j ACCEPT -p tcp -dport 443 --dst-range 13.107.9.0-13.107.9.254
+run_cmd iptables -A OUTPUT -j ACCEPT -p tcp -dport 443 --dst-range 13.107.42.0-13.107.43.254
 run_cmd iptables -A OUTPUT -j ACCEPT -p tcp --dport 22
 run_cmd iptables -A OUTPUT -j ACCEPT -m owner --uid-owner proxy
 run_cmd iptables -A OUTPUT -j DROP ! -o lo+ -m state --state NEW -p tcp
